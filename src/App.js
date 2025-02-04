a3bdefa294496ff3443975b0295fc79ab8e3d29b
lopesma11 <ml722090@gmail.com>
2025-02-04 10:16:11 -0300

    task: working with states

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import Header from "./Header";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: Math.random(),
+      title: "Title01",
+      subtitle: "Sub01",
+      likes: 20,
+    },
+    {
+      id: Math.random(),
+      title: "Title02",
+      subtitle: "Sub02",
+      likes: 10,
+    },
+    {
+      id: Math.random(),
+      title: "Title03",
+      subtitle: "Sub03",
+      likes: 50,
+    },
+  ]);
+
+  const handleRefresh = () => {
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Title0${posts.length + 1}`,
+        subtitle: `Sub0${posts.length + 1}`,
+        likes: 50,
+      },
+    ]);
+  };
   return (
     <>
       <Header>
-        <h2>Posts da Semana</h2>
+        <h2>
+          Posts da Semana
+          <button onClick={handleRefresh}>Ataulizar Página</button>
+        </h2>
       </Header>
 
-      <hr></hr>
+      <hr />
 
-      <Post
-        likes={20}
-        post={{
-          title: { tituloZeroUm },
-          subtitle: "Subtítulo da Notícia 01",
-        }}
-      />
-
-      <Post
-        likes={45}
-        post={{
-          title: { tituloZeroUm },
-          subtitle: "Subtítulo da Notícia 01",
-        }}
-      />
-
-      <Post
-        likes={50}
-        post={{
-          title: { tituloZeroUm },
-          subtitle: "Subtítulo da Notícia 01",
-        }}
-      />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          likes={post.likes}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle,
+          }}
+        />
+      ))}
     </>
   );
 }
